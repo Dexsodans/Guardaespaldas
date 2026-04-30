6e27a70ac043690ebe905993fc63be5044f5f0ed
Dexsodans <danilopache1@gmail.com>
2026-04-29 20:57:54 -0400

    cubo, cursos y colisiones ya fucionado en dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import { Canvas } from '@react-three/fiber';
 import { Obstacles } from './components/game/Obstacles';
-import './App.css'; // <--- ¡Asegúrate de que esta línea esté!
+import VIPBox from './components/game/movimiento/VIPBox';
+import CustomCursor from './components/game/movimiento/CustomCursor';
+import './App.css';
 
 function App() {
   return (
-    <div className="canvas-container">
+    <div className="canvas-container" style={{ position: "relative", width: "100vw", height: "100vh" }}>
+      
+      {/* Canvas de Three.js */}
       <Canvas camera={{ position: [0, 0, 5], fov: 75 }}>
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 10, 10]} />
+
+        {/* Obstáculos */}
         <Obstacles />
+
       </Canvas>
+
+      {/* Cursor personalizado */}
+      <CustomCursor />
+
+      {/* Personaje draggable */}
+      <VIPBox />
+
     </div>
   );
 }
