c280cb0dec23a2301df2a00b2e23d8203c6bf991
Sebaxlord64 <mauriciovasgui924@gmail.com>
2026-04-29 20:32:24 -0400

    Agrega contadores de tiempo y colisiones

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,32 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import Timer from './components/game/Contadores/Timer'
+import CollisionCounter from './components/game/Contadores/CollisionCounter'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [collisions, setCollisions] = useState(0)
   return (
     <>
+      <Timer isRunning={true} />
+      <CollisionCounter collisions={collisions} />
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
+
         <div>
           <h1>Get started</h1>
+
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
         </div>
+
         <button
           type="button"
           className="counter"
@@ -28,7 +35,13 @@ function App() {
         >
           Count is {count}
         </button>
-      </section>
+        <button
+        type="button"
+        onClick={() => setCollisions((prev) => prev + 1)}
+        >
+        Simular choque
+        </button>
+        </section>
 
       <div className="ticks"></div>
 
@@ -37,8 +50,11 @@ function App() {
           <svg className="icon" role="presentation" aria-hidden="true">
             <use href="/icons.svg#documentation-icon"></use>
           </svg>
+
           <h2>Documentation</h2>
+
           <p>Your questions, answered</p>
+
           <ul>
             <li>
               <a href="https://vite.dev/" target="_blank">
@@ -46,6 +62,7 @@ function App() {
                 Explore Vite
               </a>
             </li>
+
             <li>
               <a href="https://react.dev/" target="_blank">
                 <img className="button-icon" src={reactLogo} alt="" />
@@ -54,12 +71,16 @@ function App() {
             </li>
           </ul>
         </div>
+
         <div id="social">
           <svg className="icon" role="presentation" aria-hidden="true">
             <use href="/icons.svg#social-icon"></use>
           </svg>
+
           <h2>Connect with us</h2>
+
           <p>Join the Vite community</p>
+
           <ul>
             <li>
               <a href="https://github.com/vitejs/vite" target="_blank">
@@ -70,9 +91,11 @@ function App() {
                 >
                   <use href="/icons.svg#github-icon"></use>
                 </svg>
+
                 GitHub
               </a>
             </li>
+
             <li>
               <a href="https://chat.vite.dev/" target="_blank">
                 <svg
@@ -82,9 +105,11 @@ function App() {
                 >
                   <use href="/icons.svg#discord-icon"></use>
                 </svg>
+
                 Discord
               </a>
             </li>
+
             <li>
               <a href="https://x.com/vite_js" target="_blank">
                 <svg
@@ -94,9 +119,11 @@ function App() {
                 >
                   <use href="/icons.svg#x-icon"></use>
                 </svg>
+
                 X.com
               </a>
             </li>
+
             <li>
               <a href="https://bsky.app/profile/vite.dev" target="_blank">
                 <svg
@@ -106,6 +133,7 @@ function App() {
                 >
                   <use href="/icons.svg#bluesky-icon"></use>
                 </svg>
+
                 Bluesky
               </a>
             </li>
@@ -114,6 +142,7 @@ function App() {
       </section>
 
       <div className="ticks"></div>
+
       <section id="spacer"></section>
     </>
   )
